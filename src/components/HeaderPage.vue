0ca165621d2db21eb6a12e946a5cf7714773cb7c
SimoCrash <simone.crescenzi@hotmail.com>
2022-11-30 16:00:30 +0100

    struttura components

---
+++ src/components/HeaderPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="container-header">
+        <img src="https://boolflix.netlify.app/img/logo.png" alt="">
+        <input type="text" placeholder="Cerca film...">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'HeaderPage',
+
+}
+</script>
+
+<style lang="scss" scoped>
+    .container-header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        background-color: black;
+        height: 15vh;
+        img, input {
+            margin: 1rem;
+        }
+    }
+</style>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'MainPage',
+}
+</script>
+
+<style lang="scss" scoped>
+    div {
+        height: 85vh;
+        background-color: #6b6b6b;
+    }
+</style>
